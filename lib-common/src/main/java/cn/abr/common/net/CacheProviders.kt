1f64ba0b4cfde4cf2fe4e1fd4282548388c6575e
shixiansheng <wfengtech@163.com>
2019-07-16 18:11:40 +0800

    架构完成

---
+++ lib-common/src/main/java/cn/abr/common/net/CacheProviders.kt
@@ -0,0 +1,27 @@
+package cn.abr.common.net
+
+import io.reactivex.Flowable
+import io.rx_cache2.*
+import java.util.concurrent.TimeUnit
+
+/**
+ *
+ * 版权：汽场(北京)信息科技有限公司 版权所有
+ *
+ * 作者：时志邦
+ *
+ * 创建日期：2019/4/25/025
+ *
+ * 描述：
+ *
+ */
+interface CacheProviders {
+   /* @LifeCache(duration = 1, timeUnit = TimeUnit.MINUTES)
+    @ProviderKey("hotBrands")
+    fun hotBrands(
+        hotBrands: Flowable<List<HotBrandsEntity>>
+        , dynamicKey: DynamicKey
+        , evictProvider: EvictProvider
+    ): Flowable<List<HotBrandsEntity>>*/
+
+}
